fe34a90a1ba26652d1b8609b6baccbd3edd7621d
anuj-thankur-513 <anujthakur2003.anuj@gmail.com>
2023-04-02 01:11:35 +0530

    feat: add context for state management

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav
+      className="navbar navbar-light justify-content-center fs-3 mb-5"
+      style={{ backgroundColor: '#b2b2ac' }}
+    >
+      <strong>ADMIN PANEL</strong>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ client/src/Context/BookingContext.jsx
@@ -0,0 +1,101 @@
+import { createContext, useState } from 'react'
+import { API_HOST } from '../Config/API'
+
+export const bookingContext = createContext()
+
+const BookingContextProvider = ({ children }) => {
+  const [booking, setBooking] = useState([])
+
+  // Get all the users
+  const getBookings = async () => {
+    // API CALL
+    const response = await fetch(`${API_HOST}/bookings`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const json = await response.json()
+    setBooking(json)
+  }
+  // Adding a user
+  const addBooking = async (
+    email,
+    roomNumber,
+    roomType,
+    startTime,
+    endTime,
+    callback
+  ) => {
+    const response = await fetch(`${API_HOST}/bookings`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        roomNumber,
+        roomType,
+        startTime,
+        endTime,
+      }),
+    })
+    if (response.status === 409) {
+      callback(false)
+    } else {
+      const json = await response.json()
+      console.log(json)
+      setBooking(users.concat(json))
+      callback(true)
+    }
+  }
+
+  // Deleting a user
+  const deleteBooking = async (id) => {
+    await fetch(`${API_HOST}/bookings/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    getBookings()
+  }
+
+  // Edit a user
+  const editBooking = async (
+    id,
+    email,
+    roomNumber,
+    roomType,
+    startTime,
+    endTime
+  ) => {
+    // Logic to edit in backend
+    await fetch(`${API_HOST}/bookings/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        roomNumber,
+        roomType,
+        startTime,
+        endTime,
+      }),
+    })
+
+    // Logic to edit the code in the frontend
+    getBookings()
+  }
+
+  return (
+    <bookingContext.Provider
+      value={{ booking, addBooking, deleteBooking, editBooking, getBookings }}
+    >
+      {children}
+    </bookingContext.Provider>
+  )
+}
+
+export default BookingContextProvider
