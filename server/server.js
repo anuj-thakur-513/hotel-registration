fb4057ebf1aae98d3048913254d275c65a63d8ea
anuj-thankur-513 <anujthakur2003.anuj@gmail.com>
2023-04-01 21:01:36 +0530

    DB connection added and cors middleware added

--- server/server.js
+++ server/server.js
@@ -5,10 +5,25 @@ const mongoose = require('mongoose')
 
 require('dotenv').config()
 
-const { PORT } = process.env
-
+const { PORT, DB_URL } = process.env
 const app = express()
+// cors middleware
+app.use(cors())
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`)
-})
+// bodyparser middleware
+app.use(bodyParser.json())
+
+mongoose
+  .connect(DB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('DB is connected')
+    app.listen(PORT || 5000, () => {
+      console.log(`Starting server at port ${PORT || 5000}`)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
